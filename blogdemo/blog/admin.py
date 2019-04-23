c8ee826017fbc5e54bb667149e9acec7f5633d4d
wangshuo <670681219@qq.com>
2019-04-23 18:04:25 +0800

    创建数据库并插入数据

--- blogdemo/blog/admin.py
+++ blogdemo/blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+# class ArticleAdmin(admin.ModelAdmin):
+
+
+admin.site.register(Article)
+admin.site.register(Sort)
+admin.site.register(Tag)
+admin.site.register(Comment)
+
--- blogdemo/blog/models.py
+++ blogdemo/blog/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    auther = models.CharField(max_length=64)
+    abstract = models.TextField()
+    content = models.TextField()
+    readnum = models.IntegerField(default=0)
+    datetime = models.DateTimeField(auto_now_add=True,null=True,blank=True)
+    sortid = models.ForeignKey('Sort',on_delete=models.CASCADE)
+    h = models.ManyToManyField(to='Tag')
+
+    def __str__(self):
+        return self.title
+
+class Sort(models.Model):
+    sortname = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.sortname
+
+class Tag(models.Model):
+    tagname = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.tagname
+
+class Comment(models.Model):
+    username = models.CharField(max_length=20)
+    email = models.CharField(max_length=20)
+    website = models.CharField(max_length=40)
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True,null=True,blank=True)
+    articleid = models.ForeignKey('Article',on_delete=models.CASCADE)
+
+
+
+
+
