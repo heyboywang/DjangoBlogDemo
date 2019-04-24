2133a73a99b6cfe259bcfcc84700503fbe840555
wangshuo <670681219@qq.com>
2019-04-24 11:27:00 +0800

    实现根据归档（年月），分类，标签查找文章

--- blogdemo/blog/urls.py
+++ blogdemo/blog/urls.py
@@ -6,6 +6,9 @@ app_name = "blog"
 
 urlpatterns = [
     url(r'^$',views.index,name="index"),
+    url(r'^fullwidth/$',views.fullwidth,name="fullwidth"),
     url(r'^single/(\d+)/$',views.single,name="single"),
-
+    url(r'^sarticle/(\d+)/$',views.sortarticle,name="sarticle"),
+    url(r'^tarticle/(\d+)/$',views.tagarticle,name="tarticle"),
+    url(r'^marticle/(.*?)/(.*?)/$',views.montharticle,name="marticle"),
 ]
--- blogdemo/blog/views.py
+++ blogdemo/blog/views.py
@@ -9,7 +9,37 @@ def index(request):
     articles = Article.objects.all()
     sorts = Sort.objects.all()
     tags = Tag.objects.all()
-    return render(request,"blogtemplates/index.html",{"articles":articles,"sorts":sorts,"tags":tags})
+    datenow = datetime.datetime.now()
+    # print(type(datenow.year))
+    return render(request,"blogtemplates/index.html",{"articles":articles,"sorts":sorts,"tags":tags,"datenow":datenow})
+
+def fullwidth(request):
+    articles = Article.objects.all()
+    return render(request, "blogtemplates/full-width.html",{"articles": articles})
+
+def sortarticle(request,i):
+    sorts = Sort.objects.all()
+    tags = Tag.objects.all()
+    datenow = datetime.datetime.now()
+    sarticles = Sort.objects.get(pk=int(i)).article_set.all()
+    return render(request, "blogtemplates/index.html", {"articles": sarticles, "sorts": sorts, "tags": tags,"datenow":datenow})
+
+def tagarticle(request,i):
+    sorts = Sort.objects.all()
+    tags = Tag.objects.all()
+    datenow = datetime.datetime.now()
+    tarticles = Tag.objects.get(pk=int(i)).article_set.all()
+    return render(request, "blogtemplates/index.html", {"articles": tarticles, "sorts": sorts, "tags": tags,"datenow":datenow})
+
+def montharticle(request,y,m):
+    sorts = Sort.objects.all()
+    tags = Tag.objects.all()
+    datenow = datetime.datetime.now()
+    # print(y,type(y))
+    montharticles = Article.objects.all().filter(datetime__year = y) .filter(datetime__month = m)
+    # print(str(montharticles),type(montharticles))
+    return render(request, "blogtemplates/index.html", {"articles": montharticles, "sorts": sorts, "tags": tags,"datenow":datenow})
+    # return HttpResponse("aaaaaaa")
 
 def single(request,id):
     if request.method == "POST":
@@ -28,11 +58,13 @@ def single(request,id):
         comment.save()
 
     articles = Article.objects.all()
+    sorts = Sort.objects.all()
+    tags = Tag.objects.all()
     article = Article.objects.get(pk=int(id))
     article.readnum += 1
     article.save()
     comments =article.comment_set.all()
 
     # return HttpResponseRedirect("/single/"+str(article.id)+"/",{"article":article,'comments':comments})
-    return render(request,"blogtemplates/single.html",{"article":article,'comments':comments,"articles":articles})
+    return render(request,"blogtemplates/single.html",{"article":article,'comments':comments,"articles":articles, "sorts": sorts, "tags": tags})
     # return HttpResponse("成功")
