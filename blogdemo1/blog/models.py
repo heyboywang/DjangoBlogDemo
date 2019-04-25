4d99d82658b405b0eec1f0357ac30f013b373110
wangshuo <670681219@qq.com>
2019-04-25 11:35:25 +0800

    创建模型 并生成、应用迁移文件

--- blogdemo1/blog/models.py
+++ blogdemo1/blog/models.py
@@ -1,4 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    auther = models.CharField(max_length=64)
+    abstract = models.TextField()
+    content = models.TextField()
+    readnum = models.IntegerField(default=0)
+    datetime = models.DateTimeField(auto_now_add=True,null=True,blank=True)
+    sortid = models.ForeignKey('Sort',on_delete=models.CASCADE)
+    a_u = models.OneToOneField(User,on_delete=models.CASCADE)
+    a_t = models.ManyToManyField(to='Tag')
 
+    def __str__(self):
+        return self.title
+
+class Sort(models.Model):
+    sortname = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.sortname
+
+class Tag(models.Model):
+    tagname = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.tagname
